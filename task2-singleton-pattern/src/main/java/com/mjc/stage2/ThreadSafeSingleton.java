1dd0e6f939d5cefa5b4ec14c60ff7f1b1038d944
Nikita Pyshchev <npyshchev@igatec.com>
2025-02-13 18:19:43 +0300

    fx

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,7 +1,7 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    private static final ThreadSafeSingleton instance = new ThreadSafeSingleton();
+    private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
 
     private ThreadSafeSingleton() {
     }
